2da0185a6cb3b547a006a380f0f65fb4eb484dcc
Clouddelta <ClouddeltaJia@gmail.com>
2021-12-03 20:41:01 -0600

    。。

--- app.R
+++ app.R
@@ -1,9 +1,12 @@
 library(dplyr)
 library(shiny)
 library(ggplot2)
+library(TSA)
+library(tseries)
+library(tidyr)
 covid_data=read.csv(url("https://covid19.who.int/WHO-COVID-19-global-data.csv"))
 names(covid_data)[1]='Date'
-canada_covid=covid_data%>%filter(Country=='Canada')
+canada_covid=covid_data%>%filter(Date>='2020-12-29'&Country=='Canada')
 
 #doesn't work
 #download.file('https://covid19.who.int/who-data/vaccination-data.csv',
@@ -20,8 +23,8 @@ ui <- fluidPage(
                     sliderInput(inputId = "days",
                       label = "Number of days:",
                       min = 2,
-                      max = 10,
-                      value = 5)
+                      max = 5,
+                      value = 3)
                               ),
        mainPanel(
           plotOutput("plot")
@@ -31,21 +34,57 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   output$plot <- renderPlot({
       l=input$days
+      ####deaths prediction by ts
+      V_neg.log.return<-canada_covid$New_deaths
+      V_neg.log.return<-log(V_neg.log.return+1)
+      #V_neg.log.return<--log(V_neg.log.return)
+      #Split the last five values
+      V_last.L=V_neg.log.return[(length(V_neg.log.return)-l+1):(length(V_neg.log.return))]
+      V_without.last.five=V_neg.log.return[1:(length(V_neg.log.return)-l)]
+      Min=10000
+      P=0
+      D=0
+      Q=0
+      actual = V_last.L
+      for(p in 0:4){
+        for(d in 0:1){
+          for(q in 0:4){
+            model <- arima(V_without.last.five, order=c(p,d,q))
+            forecast <- predict(model,n.ahead=l)$pred
+            MSFE<-sum((forecast-actual)^2/l)
+            if(MSFE<Min){
+              Min=MSFE
+              P=p
+              D=d
+              Q=q
+            }
+          }
+        }
+      }
+      #c(P,D,Q)
+      TS<-arima(V_without.last.five, order=c(P,D,Q))
+      #TS$coef
+      predict_deaths=exp(predict(TS,n.ahead=l)$pred)-1
       last_five_day_deaths=tail(canada_covid,n=l)%>%select(Date,New_deaths)
-      test=last_five_day_deaths
-      test[,2]=canada_covid[c(81:(80+l)),]%>%select(New_deaths)
-      names(test)[2]='predict_deaths'
-      canada_deaths_data=left_join(last_five_day_deaths,test)
+      last_thirty=tail(canada_covid,n=30+l)%>%
+        select(Date,New_deaths)%>%
+        mutate(predict_deaths=New_deaths)%>%
+        head(-l)
+#      test=last_five_day_deaths
+#      test[,2]=canada_covid[c(81:(80+l)),]%>%select(New_deaths)
+#      names(test)[2]='predict_death'
+      canada_deaths_data=last_five_day_deaths%>%
+        cbind(predict_deaths)%>%
+        bind_rows(last_thirty)%>%arrange(Date)
       ggplot(data=canada_deaths_data,aes(x=as.Date(Date)))+
+        geom_line(aes(y=predict_deaths,colour='predict_deaths'))+
       geom_line(aes(y=New_deaths,colour='New_deaths'))+
-      geom_line(aes(y=predict_deaths,colour='ratio'))+
-      scale_x_date(date_labels = "%m-%Y")+xlab("") +ylab("") +
+      scale_x_date(date_labels = "%m-%d")+xlab("") +ylab("") +
       theme_minimal()+
       scale_colour_manual('',
                           breaks=c('New_deaths','predict_deaths'),
-                          values=c('New_deaths'="#69b3a2",'predict_deaths'="steelblue"))+
-      geom_vline(xintercept = as.Date("2021-09-07"),color='red',linetype='dashed')
-
-    })
+                          values=c('New_deaths'="steelblue",'predict_deaths'='#69b3a2')
+                          )
+  })
 }
 shinyApp(ui, server)
